4a6484dd8708e2d2b6ae91ceacf670f0c47361b7
Srinath <srnthsrdhrn1@gmail.com>
2016-11-25 20:46:44 +0530

    Finished testing

--- app/src/main/java/io/srinath/testing/MainActivity.java
+++ app/src/main/java/io/srinath/testing/MainActivity.java
@@ -43,22 +43,11 @@ public class MainActivity extends AppCompatActivity{
 
                 @Override
                 public void onFailure(Call<List<Events>> call, Throwable t) {
-
+                    EventBus.getDefault().post("Failure");
                 }
             });
             return null;
         }
     }
 
-    @Override
-    protected void onStart() {
-        super.onStart();
-        EventBus.getDefault().register(this);
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        EventBus.getDefault().unregister(this);
-    }
 }
